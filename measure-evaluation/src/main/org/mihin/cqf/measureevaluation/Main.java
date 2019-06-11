3d664dbf011eaeced69b18ff82d5b24dd19d4543
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2019-06-11 13:28:23 -0700

    Updated to connectathon22 instance Added Patient-411 collect data response

--- measure-evaluation/src/main/org/mihin/cqf/measureevaluation/Main.java
+++ measure-evaluation/src/main/org/mihin/cqf/measureevaluation/Main.java
@@ -12,49 +12,91 @@ import java.io.Console;
 public class Main {
 
     public static void main(String[] args) {
+        setup();
+        retrievePatient();
+        evaluateIndividualMeasure();
+    }
+
+    private static FhirContext ctx;
+    private static IParser parser;
+    private static IGenericClient client;
+
+    private static void setup() {
         // Build a DSTU3 context
-        FhirContext ctx = FhirContext.forDstu3();
-        IParser parser = ctx.newXmlParser().setPrettyPrint(true);
+        ctx = FhirContext.forDstu3();
+        parser = ctx.newXmlParser().setPrettyPrint(true);
 
         // Create a new client using the desired endpoint
-        IGenericClient client = ctx.newRestfulGenericClient("http://measure.eval.kanvix.com/cqf-ruler/baseDstu3");
+        client = ctx.newRestfulGenericClient("http://connectathon22.alphora.com/cqf-ruler/baseDstu3");
+    }
 
-
+    private static void retrievePatient() {
         // Retrieve a Patient
-        Patient patient = client.read().resource(Patient.class).withId("Patient-1153").execute();
+        Patient patient = client.read().resource(Patient.class).withId("Patient-411").execute();
 
         // Write the patient out as an XML string
         System.out.println(parser.encodeResourceToString(patient));
+    }
+
+    private static void evaluateIndividualMeasure() {
 
         // Execute a measure evaluation for the patient
         Parameters response = client.operation().onInstance(new IdType("Measure", "measure-bcs"))
                 .named("evaluate-measure")
-                .withParameter(Parameters.class, "patient", new StringType("Patient-1153"))
-                .andParameter("periodStart", new DateTimeType("2017-01"))
-                .andParameter("periodEnd", new DateTimeType("2017-12"))
+                .withParameter(Parameters.class, "patient", new StringType("Patient-411"))
+                .andParameter("periodStart", new DateTimeType("2018-01-01"))
+                .andParameter("periodEnd", new DateTimeType("2018-12-31"))
+                .useHttpGet()
+                .execute();
+
+        // Write out the result as an XML string
+        System.out.println(parser.encodeResourceToString(response));
+    }
+
+    private static void collectData() {
+
+        // Collect data for the patient
+        Parameters response = client.operation().onInstance(new IdType("Measure", "measure-bcs"))
+                .named("collect-data")
+                .withParameter(Parameters.class, "patient", new StringType("Patient-411"))
+                .andParameter("periodStart", new DateTimeType("2018-01-01"))
+                .andParameter("periodEnd", new DateTimeType("2018-12-31"))
                 .useHttpGet()
                 .execute();
 
         // Write out the result as an XML string
         System.out.println(parser.encodeResourceToString(response));
 
-//        // Execute a measure evaluation for the population
-//        response = client.operation().onInstance(new IdType("Measure", "measure-bcs"))
-//                .named("evaluate-measure")
-//                .withParameter(Parameters.class, "periodStart", new DateTimeType("2017-01"))
-//                .andParameter("periodEnd", new DateTimeType("2017-12"))
-//                .useHttpGet()
-//                .execute();
+        // Submit the results (POST $submit-data)
+        //Parameters submitResponse = client.operation().onInstance(new IdType("Measure", "measure-bcs"))
+        //      .named("submit-data")
+        //        .withParameters((Object)response)
+    }
+
+    private static void submitData() {
+
+    }
+
+    private static void evaluateSummaryMeasure() {
+        // Execute a measure evaluation for the population
+        Parameters response = client.operation().onInstance(new IdType("Measure", "measure-bcs"))
+                .named("evaluate-measure")
+                .withParameter(Parameters.class, "periodStart", new DateTimeType("2018-01"))
+                .andParameter("periodEnd", new DateTimeType("2018-12"))
+                .useHttpGet()
+                .execute();
 
         // Write out the result as an XML string
         System.out.println(parser.encodeResourceToString(response));
+    }
 
+    private static void careGaps() {
         // Execute a care gaps report for the patient
-        response = client.operation().onType(Measure.class)
+        Parameters response = client.operation().onType(Measure.class)
                 .named("care-gaps")
-                .withParameter(Parameters.class, "patient", new StringType("Patient-1153"))
-                .andParameter("periodStart", new DateTimeType("2017-01"))
-                .andParameter("periodEnd", new DateTimeType("2017-12"))
+                .withParameter(Parameters.class, "patient", new StringType("Patient-411"))
+                .andParameter("periodStart", new DateTimeType("2018-01-01"))
+                .andParameter("periodEnd", new DateTimeType("2018-12-31"))
                 .andParameter("topic", new StringType("Preventive Care and Screening"))
                 .useHttpGet()
                 .execute();
